78f2c8b1d7b92b0294c0133b9933aa172e12e6d7
Martin Lercher <lercher@singhammer.de>
2017-09-20 15:15:53 +0200

    initial version

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lercher.MonitorDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("This is test program for the MonitorDictionary class.");
+            System.Console.WriteLine("(C) 2018 by Martin Lercher");
+            System.Console.WriteLine();
+        }
+    }
+}
